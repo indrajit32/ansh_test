cebc125e539aceac6013b8a14babcaa344cc6c53
Indrajit Auddy <z.indrajit.auddy@gree.net>
2018-11-29 20:17:58 +0530

    Test 1

---
+++ calculator.php
@@ -0,0 +1,31 @@
+<?php
+
+class Calculator{
+
+	public function sum($num) {
+
+		if(count($num)  > 0)
+			return array_sum($num);
+		else
+			return 0;
+	}
+
+	public function format($raw,$delimeter = ","){
+
+		$data = explode($delimeter,$raw);
+		return $data;
+	} 
+}
+
+$obj = new Calculator;
+
+switch( $argv[1] ){
+
+	case "sum":
+		$num_arr = $obj->format( $argv[2] );
+		echo $obj->sum($num_arr);
+	break;
+
+}
+
+?>
